26688b407b9bf1ad675d89a722c955f8b75838ce
Viktor Dombrowski <47988211+viktor1234678@users.noreply.github.com>
2026-01-11 12:40:48 +0100

    Update example_code.py

--- example_code.py
+++ example_code.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 """
-PROPER STATE TRACKING - No flicker
-Spacebar pressed = Black screen, released = White screen
-400x240 Sharp Memory Display
+SIMPLE AND STABLE - No flicker guaranteed
 """
 
 import board
@@ -13,68 +11,55 @@ import sys
 import termios
 import tty
 import select
-import time
 
-# Display setup
+# Setup
 spi = busio.SPI(board.SCK, MOSI=board.MOSI)
 scs = digitalio.DigitalInOut(board.D6)
 display = adafruit_sharpmemorydisplay.SharpMemoryDisplay(spi, scs, 400, 240)
 
-# Save terminal
+# Terminal
 old_settings = termios.tcgetattr(sys.stdin)
-tty.setcbreak(sys.stdin.fileno())  # Use cbreak instead of raw for better handling
+tty.setcbreak(sys.stdin.fileno())
 
-# State
-display_state = 1  # 1=white, 0=black
-key_state = 0      # 0=no key, 32=space, 3=ctrl-c
+print("Press SPACE for black, release for white")
+print("Ctrl-C to exit")
 
-# Start with white
+# Start white
 display.fill(1)
 display.show()
 
-print("Running. Space=Black, Release=White, Ctrl-C to exit")
-
 try:
     while True:
-        # Check for keypress
-        ready, _, _ = select.select([sys.stdin], [], [], 0.01)  # 10ms timeout
+        # Wait for any key
+        select.select([sys.stdin], [], [])
+        key = sys.stdin.read(1)
         
-        if ready:
-            # Read key
-            try:
-                key = ord(sys.stdin.read(1))
-            except TypeError:
-                continue
+        if key == '\x03':  # Ctrl-C
+            break
             
-            if key == 3:  # Ctrl-C
-                break
-            elif key == 32:  # Space
-                if key_state != 32:  # Space newly pressed
-                    key_state = 32
-                    if display_state != 0:
-                        display.fill(0)
-                        display.show()
-                        display_state = 0
-            else:
-                # Any other key means space is released
-                if key_state == 32:  # Space was pressed, now released
-                    key_state = 0
-                    if display_state != 1:
-                        display.fill(1)
-                        display.show()
-                        display_state = 1
-        else:
-            # No key pressed
-            if key_state == 32:  # Space was pressed but no input now
-                key_state = 0
-                if display_state != 1:
+        if key == ' ':
+            # Turn black immediately
+            display.fill(0)
+            display.show()
+            
+            # Keep checking until space is released
+            while True:
+                select.select([sys.stdin], [], [])
+                next_key = sys.stdin.read(1)
+                
+                if next_key == '\x03':
                     display.fill(1)
                     display.show()
-                    display_state = 1
+                    sys.exit(0)
+                    
+                if next_key != ' ':  # Space released
+                    break
+            
+            # Turn white
+            display.fill(1)
+            display.show()
             
 finally:
-    # Cleanup
     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
     display.fill(1)
     display.show()
-    print("\nExited")
