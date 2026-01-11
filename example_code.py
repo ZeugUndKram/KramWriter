83034a7808dab850defcb2c206ae65ad182ff982
Viktor Dombrowski <47988211+viktor1234678@users.noreply.github.com>
2026-01-11 12:45:32 +0100

    Update example_code.py

--- example_code.py
+++ example_code.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """
-DEBOUNCED - No flicker, simple and reliable
+SIMPLE AND STABLE - Fixed release detection
+Spacebar pressed = Black screen, released = White screen
+400x240 Sharp Memory Display
 """
 
 import board
@@ -12,17 +14,17 @@ import termios
 import tty
 import select
 
-# Display
+# Setup
 spi = busio.SPI(board.SCK, MOSI=board.MOSI)
 scs = digitalio.DigitalInOut(board.D6)
 display = adafruit_sharpmemorydisplay.SharpMemoryDisplay(spi, scs, 400, 240)
 
-# Terminal setup
+# Terminal
 old_settings = termios.tcgetattr(sys.stdin)
 tty.setcbreak(sys.stdin.fileno())
 
-# State
-screen_is_black = False
+print("Press SPACE for black, release for white")
+print("Ctrl-C to exit")
 
 # Start white
 display.fill(1)
@@ -30,39 +32,41 @@ display.show()
 
 try:
     while True:
-        # Wait for key event (blocking until keypress)
+        # Wait for any key
         select.select([sys.stdin], [], [])
-        
-        # Read the key
         key = sys.stdin.read(1)
         
         if key == '\x03':  # Ctrl-C
             break
-        elif key == ' ':
-            # Space pressed - turn black
-            if not screen_is_black:
-                display.fill(0)
-                display.show()
-                screen_is_black = True
             
-            # Wait for space release
+        if key == ' ':
+            # Turn black immediately
+            display.fill(0)
+            display.show()
+            print("Screen: BLACK (holding...)")
+            
+            # Wait for space release OR another key
             while True:
                 select.select([sys.stdin], [], [])
-                release_key = sys.stdin.read(1)
-                if release_key == '\x03':
+                next_key = sys.stdin.read(1)
+                
+                if next_key == '\x03':
                     display.fill(1)
                     display.show()
                     sys.exit(0)
-                elif release_key != ' ':  # Any non-space key means space released
-                    break
+                    
+                # If we get ANY key (including another space), break and check
+                # In practice, space release sends no character, so any character
+                # means the user pressed a different key
+                break
             
-            # Space released - turn white
-            if screen_is_black:
-                display.fill(1)
-                display.show()
-                screen_is_black = False
-                
+            # Turn white (space was released or another key was pressed)
+            display.fill(1)
+            display.show()
+            print("Screen: WHITE")
+            
 finally:
     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
     display.fill(1)
     display.show()
+    print("\nExited")
