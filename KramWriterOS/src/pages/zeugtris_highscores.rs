7722640562afce758dea7560a56ef64a016cadd1
Viktor Dombrowski <47988211+viktor1234678@users.noreply.github.com>
2026-05-01 04:59:12 +0200

    Update zeugtris_highscores.rs

--- KramWriterOS/src/pages/zeugtris_highscores.rs
+++ KramWriterOS/src/pages/zeugtris_highscores.rs
@@ -76,7 +76,7 @@ impl Page for ZeugtrisHighscoresPage {
 
         // Display Scores in a two-column layout
         let font_size = 32.0;
-        let start_y = 80;
+        let start_y = 105;
         let spacing_y = 28;
         
         let col1_x = 70;
